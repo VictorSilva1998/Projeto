78abb5de7d18087fa33cb3c7b20615851ce17ac9
VictorSilva1998 <victor.alexandre9813@gmail.com>
2026-07-15 14:39:40 -0400

    Atividade do Mauricio

--- Pasta de PySide6/Torre de Hanoi.py
+++ Pasta de PySide6/Torre de Hanoi.py
@@ -1,10 +1,18 @@
+def torre_hanoi(n, origem, destino, auxiliar):
+    if n == 1:
+        print(f"Mova o disco 1 de {origem} para {destino}")
+        return
+    torre_hanoi(n-1, origem, auxiliar, destino)
+    print(f"Mova o disco {n} de {origem} para {destino}")
+    torre_hanoi(n-1, auxiliar, destino, origem)
+
 while True:
     try:
-        n = int (input ("Digite o número de Discos na Torre de Hanoi: "))
-        break
+        if __name__=='__main__':
+            n = int (input ("Digite o número de Discos na Torre de Hanoi: "))
+            movimentos = 2**n - 1
+            print (f"Para realizar a Torre de Hanói com {n} discos serão necessários {movimentos} movimentos.")
+            torre_hanoi(n, "A", "C", "B")
+            break
     except ValueError:
         print ("\nUtilize apenas números inteiros!\n")
-
-movimentos = 2**n - 1
-
-print (f"\nPara realizar a Torre de Hanói com {n} discos serão necessários {movimentos} movimentos.")
